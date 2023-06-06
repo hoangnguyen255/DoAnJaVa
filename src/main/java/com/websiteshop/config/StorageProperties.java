0892a0d20725e4ec4587820ca002d6040d2317a3
nguyễn hoàng <hoangsino2002@gmail.com>
2023-06-06 14:50:07 +0700

    Nhom 9

---
+++ src/main/java/com/websiteshop/config/StorageProperties.java
@@ -0,0 +1,24 @@
+package com.websiteshop.config;
+
+import java.net.URI;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@ConfigurationProperties("storage")
+@Data
+public class StorageProperties {
+
+	private String location;
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	
+}
